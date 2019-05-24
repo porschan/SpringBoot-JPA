9d03aa4bd42c4293fb5e17044e7b2034a957f450
porschan <710437653@qq.com>
2019-05-24 10:23:10 +0800

    add springboot-demo

---
+++ demo/src/main/java/com/chanchifeng/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.chanchifeng.demo.service.impl;
+
+import com.chanchifeng.demo.model.User;
+import com.chanchifeng.demo.repository.UserRepository;
+import com.chanchifeng.demo.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User save(User ayUser) {
+        return userRepository.save(ayUser);
+    }
+
+
+    @Override
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+        System.out.println("userId:" + id + "用户被删除");
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<User> findByName(String name){
+        return userRepository.findByName(name);
+    }
+    @Override
+    public List<User> findByNameLike(String name){
+        return userRepository.findByNameLike(name);
+    }
+    @Override
+    public List<User> findByIdIn(Collection<Integer> ids){
+        return userRepository.findByIdIn(ids);
+    }
+}
